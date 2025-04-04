24e17a3c76b7943301d205e0bf7cbe964ec5b4b2
DLPDigital <72518625+DLPDigital@users.noreply.github.com>
2025-04-04 13:15:43 +0100

    trigger deploy

--- components/Experience/index.tsx
+++ components/Experience/index.tsx
@@ -1,36 +1,31 @@
-import React from "react"
-import Hrule from "../Hrule"
+import React from "react";
+import Hrule from "../Hrule";
 
-import styles from "./Experience.module.scss"
-import JobDetails from "./JobDetails"
-import { FullPage } from "../../types/cms"
+import styles from "./Experience.module.scss";
+import JobDetails from "./JobDetails";
+import { FullPage } from "../../types/cms";
 
 type Props = {
-  experiences: FullPage["experience"]
-}
+  experiences: FullPage["experience"];
+};
 
-const Experience: React.FC<Props> = ({ experiences }) => {
-  console.log("Experience", experiences)
-  return (
-    <div className={styles.ExperienceContainer}>
-      <h2 id="experience">Experience</h2>
-      {
-        experiences.map((experience) => {
-          return (
-            <JobDetails
-              key={experience.jobTitle}
-              title={experience.jobTitle}
-              company={experience.company}
-              tenure={experience.tenure}
-              location={experience.location}
-              details={experience.details}
-            />
-          )
-        })
-      }
-      <Hrule />
-    </div>
-  )
-}
+const Experience: React.FC<Props> = ({ experiences }) => (
+  <div className={styles.ExperienceContainer}>
+    <h2 id="experience">Experience</h2>
+    {experiences.map((experience) => {
+      return (
+        <JobDetails
+          key={experience.jobTitle}
+          title={experience.jobTitle}
+          company={experience.company}
+          tenure={experience.tenure}
+          location={experience.location}
+          details={experience.details}
+        />
+      );
+    })}
+    <Hrule />
+  </div>
+);
 
-export default Experience
+export default Experience;
